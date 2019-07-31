62101a1166f84e96f7a2d6b1daf0c90dfa9933ac
William Estrela Louzeiro <wel.estrela@gmail.com>
2019-07-31 16:12:21 -0300

    PUT DELETE e GET para cliente

---
+++ src/main/java/com/nelioalves/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,57 @@
+package com.nelioalves.cursomc.dto;
+
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.domain.Cliente;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+
+public class ClienteDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+
+    private Integer id;
+    @NotEmpty(message="O nome não pode ser vazio")
+    @Length(min=5, max=80, message = "O tamanho tem que ser entre 5 e 80 caracteres")
+    private String nome;
+
+    @NotEmpty(message="O email não pode ser vazio")
+    @Email(message="Email inválido")
+    private String email;
+
+    public ClienteDTO(){
+
+    }
+
+    public ClienteDTO(Cliente obj){
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+        this.email = obj.getEmail();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- src/main/java/com/nelioalves/cursomc/resources/ClientesResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/ClientesResource.java
@@ -1,27 +1,64 @@
 package com.nelioalves.cursomc.resources;
 
-import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.domain.Cliente;
-import com.nelioalves.cursomc.services.CategoriaService;
+import com.nelioalves.cursomc.domain.Cliente;
+import com.nelioalves.cursomc.dto.ClienteDTO;
+import com.nelioalves.cursomc.services.ClienteService;
 import com.nelioalves.cursomc.services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/clientes")
 public class ClientesResource {
 
-	@Autowired
-	private ClienteService service;
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
-		Cliente obj = service.find(id);
-		return ResponseEntity.ok().body(obj);
-		
-	}
+    @Autowired
+    private ClienteService service;
+
+    @RequestMapping(value="/{id}", method=RequestMethod.GET)
+    public ResponseEntity<Cliente> find(@PathVariable Integer id) {
+        Cliente obj = service.find(id);
+        return ResponseEntity.ok().body(obj);
+
+    }
+
+    @PutMapping(value="{id}")
+    public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id){
+        Cliente obj = service.fromDTO(objDto);
+        obj.setId(id);
+        service.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping(value="/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ClienteDTO>> findAll() {
+        List<Cliente> list = service.findAll();
+        List<ClienteDTO> categoria = list.stream().map(ClienteDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(categoria);
+    }
+
+    @GetMapping(value="/page")
+    public ResponseEntity<Page<ClienteDTO>> findPage(@RequestParam(value="page", defaultValue = "0") Integer page,
+                                                     @RequestParam(value="linesPerPage", defaultValue = "24") Integer linesPerPage,
+                                                     @RequestParam(value="porderByage", defaultValue = "nome") String orderBy,
+                                                     @RequestParam(value="direction", defaultValue = "ASC") String direction) {
+
+        Page<Cliente> categorias = service.findPage(page,linesPerPage,orderBy,direction);
+
+        Page<ClienteDTO> categoriaDTO = categorias.map(ClienteDTO::new);
+        return ResponseEntity.ok().body(categoriaDTO);
+
+    }
 }
